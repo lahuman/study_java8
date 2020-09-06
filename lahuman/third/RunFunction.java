18139c714925d944abf04533b03e1b36b7677aea
Daniel Lim <lahuman@daum.net>
2020-09-06 21:16:31 +0900

    feat : stream으로 처리 연습

--- lahuman/third/RunFunction.java
+++ lahuman/third/RunFunction.java
@@ -3,6 +3,7 @@ package lahuman.third;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RunFunction{
     public static void main(String[] args){
@@ -10,10 +11,12 @@ public class RunFunction{
         l.stream().forEach(System.out::println);
     }
     public static <T, R> List<R> map(List<T> list, java.util.function.Function<T, R> f) {
-        List<R> result = new ArrayList<>();
-        for(T s : list){
-            result.add(f.apply(s));
-        }
-        return result;
+        // List<R> result = new ArrayList<>();
+        // for(T s : list){
+        //     result.add(f.apply(s));
+        // }
+        // return result;
+
+        return list.stream().map(f::apply).collect(Collectors.toList());
     }
 }
